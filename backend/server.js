b5b6b975e64a01ae68fba7e8a44a5c0cea939456
Parth <parth.manocha2901@gmail.com>
2024-07-20 19:02:35 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -19,8 +19,14 @@ const io = socketIo(server, {
   }
 });
 
-app.use(cors());
-app.use(express.json());
+app.use(cors({
+  origin: [
+    "http://localhost:3000",
+    "https://course-project-frontend-eight.vercel.app"
+  ],
+  credentials: true
+}));
+app.use(express.json());  
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
